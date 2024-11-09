4b17ef24706876f967fd5556fb640c1bc4619753
onyiimesi <onyedikachukwu62@gmail.com>
2024-11-09 10:39:04 +0100

    Staff create unique username fix

--- app/Http/Requests/StaffRequest.php
+++ app/Http/Requests/StaffRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules;
 
 class StaffRequest extends FormRequest
@@ -24,15 +26,24 @@ class StaffRequest extends FormRequest
      */
     public function rules()
     {
+        $user = Auth::user();
+        $schoolId = $user ? $user->sch_id : null;
+
         return [
             'designation_id' => ['required', 'string',],
             'department' => ['required', 'string', 'max:255'],
             'surname' => ['required', 'string', 'max:255'],
             'firstname' => ['required', 'string', 'max:255'],
             'middlename' => ['required', 'string', 'max:255'],
-            'username' => ['required', 'string', 'max:255', 'unique:staff'],
+            'username' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('staff')->where(function ($query) use ($schoolId) {
+                    return $query->where('sch_id', $schoolId);
+                })
+            ],
             'campus' => ['required', 'string', 'max:255'],
-            // 'email' => ['required', 'string', 'max:255', 'unique:staff'],
             'password' => ['string', Rules\Password::defaults()],
             'pass_word' => ['string', 'max:255'],
             'status' => ['string', 'max:20']
