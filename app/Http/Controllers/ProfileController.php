c1ce9ff222bcaf974db2369d0d5b82978a63fd08
onyiimesi <onyedikachukwu62@gmail.com>
2025-05-31 08:05:10 +0100

    Fix

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,18 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\LoginResource;
 use App\Http\Resources\StaffsResource;
 use App\Http\Resources\StudentResource;
-use App\Models\Staff;
-use App\Models\Student;
-use Illuminate\Auth\Events\Login;
+use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use ImageKit\ImageKit;
 
 class ProfileController extends Controller
 {
+    use HttpResponses;
+
     /**
      * Display a listing of the resource.
      *
@@ -78,154 +77,55 @@ class ProfileController extends Controller
      */
     public function update(Request $request)
     {
-        $stud = $request->user();
+        $user = $request->user();
+
         $imageKit = new ImageKit(
             env('IMAGEKIT_PUBLIC_KEY'),
             env('IMAGEKIT_PRIVATE_KEY'),
             env('IMAGEKIT_URL_ENDPOINT')
         );
 
-        $paths = $stud->image;
-        $pathss = $stud->signature;
+        $cleanSchId = preg_replace("/[^a-zA-Z0-9]/", "", $user->sch_id);
 
-        if($stud->designation_id != '7'){
-            // $stud->update($request->all());
-            $cleanSchId = preg_replace("/[^a-zA-Z0-9]/", "", $stud->sch_id);
+        $imageUrl = $user->image;
+        $imageFileId = $user->file_id;
 
-            if($request->image){
-                $fileId = "";
-                $file = $request->image;
-                $baseFolder = 'staff';
-                $extension = explode('/', explode(':', substr($file, 0, strpos($file, ';')))[1])[1];
-                $replace = substr($file, 0, strpos($file, ',')+1);
-                $image = str_replace($replace, '', $file);
-                $image = str_replace(' ', '+', $image);
-                $file_name = time().'.'.$extension;
-                $folderPath = $file_name;
-                $folderName = $baseFolder . '/' . $cleanSchId;
-
-                $fileId = $stud->file_id;
-                $del = $imageKit->deleteFile($fileId);
-
-                if($del){
-                    $uploadFile = $imageKit->uploadFile([
-                        'file' => $file,
-                        'fileName' => $folderPath,
-                        'folder' => $folderName
-                    ]);
-                }
-
-                $url = $uploadFile->result->url;
-                $paths = $url;
-                $fileId = $uploadFile->result->fileId;
-
-            }else{
-                $paths = $stud->image;
-                $fileId = "";
-            }
-
-            if($request->signature){
-                $sigId = "";
-                $file = $request->signature;
-                $baseFolder = 'signature';
-                $extension = explode('/', explode(':', substr($file, 0, strpos($file, ';')))[1])[1];
-                $replace = substr($file, 0, strpos($file, ',')+1);
-                $sig = str_replace($replace, '', $file);
-                $sig = str_replace(' ', '+', $sig);
-                $file_name = uniqid().'.'.$extension;
-                $folderPath = $file_name;
-                $folderName = $baseFolder . '/' . $cleanSchId;
-
-                $sigId = $stud->sig_id;
-                $imageKit->deleteFile($sigId);
-
-                $uploadFile = $imageKit->uploadFile([
-                    'file' => $file,
-                    'fileName' => $folderPath,
-                    'folder' => $folderName
-                ]);
-
-                $url = $uploadFile->result->url;
-                $pathss = $url;
-                $sigId = $uploadFile->result->fileId;
-
-            }else{
-                $pathss = "";
-                $sigId = "";
-            }
-
-            $stud->update([
-                'department' => $request->department,
-                'surname' => $request->surname,
-                'firstname' => $request->firstname,
-                'middlename' => $request->middlename,
-                'username' => $request->username,
-                'email' => $request->email,
-                'phoneno' => $request->phoneno,
-                'address' => $request->address,
-                'image' => $paths,
-                'signature' => $pathss,
-                'file_id' => $fileId,
-                'sig_id' => $sigId
-            ]);
-
-            return [
-                "status" => 'true',
-                "message" => 'Updated Successfully',
-                "data" => $stud
-            ];
-
-        }else if($stud->designation_id == '7'){
-            $cleanSchId = preg_replace("/[^a-zA-Z0-9]/", "", $stud->sch_id);
-
-            if($request->image){
-                $file = $request->image;
-                $baseFolder = 'student';
-                $extension = explode('/', explode(':', substr($file, 0, strpos($file, ';')))[1])[1];
-                $replace = substr($file, 0, strpos($file, ',')+1);
-                $image = str_replace($replace, '', $file);
-                $image = str_replace(' ', '+', $image);
-                $file_name = time().'.'.$extension;
-                $folderPath = $file_name;
-                $folderName = $baseFolder . '/' . $cleanSchId;
-
-                $fileId = $stud->file_id;
-                $imageKit->deleteFile($fileId);
-
-                $uploadFile = $imageKit->uploadFile([
-                    'file' => $file,
-                    'fileName' => $folderPath,
-                    'folder' => $folderName
-                ]);
-
-                $url = $uploadFile->result->url;
-                $paths = $url;
-                $fileId = $uploadFile->result->fileId;
-            }else{
-                $fileId = "";
-            }
-
-            $stud->update([
-                'department' => $request->department,
-                'surname' => $request->surname,
-                'firstname' => $request->firstname,
-                'middlename' => $request->middlename,
-                'username' => $request->username,
-                'email' => $request->email,
-                'phoneno' => $request->phoneno,
-                'address' => $request->address,
-                'image' => $paths,
-                'file_id' => $fileId,
-            ]);
-
-            return [
-                "status" => 'true',
-                "message" => 'Updated Successfully',
-                "data" => $stud
-            ];
-
+        if ($request->filled('image')) {
+            $imageUpload = $this->handleImagekitUpload($request->image, $imageKit, $user->file_id, $cleanSchId, $user->designation_id == '7' ? 'student' : 'staff');
+            $imageUrl = $imageUpload['url'];
+            $imageFileId = $imageUpload['fileId'];
         }
 
+        $signatureUrl = $user->signature;
+        $signatureFileId = $user->sig_id;
+
+        if ($user->designation_id != '7' && $request->filled('signature')) {
+            $signatureUpload = $this->handleImagekitUpload($request->signature, $imageKit, $user->sig_id, $cleanSchId, 'signature');
+            $signatureUrl = $signatureUpload['url'];
+            $signatureFileId = $signatureUpload['fileId'];
+        }
+
+        $updateData = [
+            'surname' => $request->surname,
+            'firstname' => $request->firstname,
+            'middlename' => $request->middlename,
+            'username' => $request->username,
+            'email' => $request->email,
+            'phoneno' => $request->phoneno,
+            'address' => $request->address,
+            'image' => $imageUrl,
+            'file_id' => $imageFileId,
+            'signature' => $user->designation_id != '7' ? $signatureUrl : null,
+            'sig_id' => $user->designation_id != '7' ? $signatureFileId : null,
+        ];
+
+        if ($user->designation_id != '7') {
+            $updateData['department'] = $request->department;
+        }
+
+        $user->update($updateData);
+
+        return $this->success($updateData, 'Profile updated successfully');
     }
 
     /**
@@ -238,4 +138,27 @@ class ProfileController extends Controller
     {
         //
     }
+
+    private function handleImagekitUpload($base64File, $imageKit, $existingFileId, $schoolId, $folder)
+    {
+        if ($existingFileId) {
+            $imageKit->deleteFile($existingFileId);
+        }
+
+        $extension = explode('/', explode(':', substr($base64File, 0, strpos($base64File, ';')))[1])[1];
+        $fileName = uniqid() . '.' . $extension;
+        $folderPath = $folder . '/' . $schoolId;
+
+        $upload = $imageKit->uploadFile([
+            'file' => $base64File,
+            'fileName' => $fileName,
+            'folder' => $folderPath
+        ]);
+
+        return [
+            'url' => $upload->result->url,
+            'fileId' => $upload->result->fileId
+        ];
+    }
+
 }
