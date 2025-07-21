ae0aa55256eddb590ea1bb08407277b6bf1bb12e
onyiimesi <onyedikachukwu62@gmail.com>
2025-07-21 15:47:04 +0100

    New fix

--- app/Http/Controllers/AssignSubjectsController.php
+++ app/Http/Controllers/AssignSubjectsController.php
@@ -8,6 +8,7 @@ use App\Models\SubjectClass;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AssignSubjectsController extends Controller
 {
@@ -25,34 +26,29 @@ class AssignSubjectsController extends Controller
             return $this->error(null, 'Academic period not set', 404);
         }
 
-        $subjects = SubjectClass::where('sch_id', $user->sch_id)
-            ->where('campus', $user->campus)
-            ->where('class_id', $request->class_id)
-            ->get();
-
         $class = ClassModel::find($request->class_id);
 
         if (! $class) {
             return $this->error(null, 'Class does not exist', 400);
         }
 
-        if (! $subjects->isEmpty()) {
-            $subjects->each(function ($subject) {
-                $subject->delete();
-            });
-        }
-
-        foreach ($request->subjects as $subjectData) {
-            SubjectClass::create([
-                'sch_id' => $user->sch_id,
-                'campus' => $user->campus,
-                'term' => $period->term,
-                'session' => $period->session,
-                'class_id' => $request->class_id,
-                'class_name' => $class->class_name,
-                'subject' => $subjectData['name']
-            ]);
-        }
+        DB::transaction(function () use ($user, $request, $period, $class) {
+            foreach ($request->subjects as $subjectData) {
+                SubjectClass::updateOrCreate(
+                    [
+                        'sch_id' => $user->sch_id,
+                        'campus' => $user->campus,
+                        'term' => $period->term,
+                        'session' => $period->session,
+                        'class_id' => $request->class_id,
+                        'subject' => $subjectData['name'],
+                    ],
+                    [
+                        'class_name' => $class->class_name,
+                    ]
+                );
+            }
+        });
 
         return $this->success(null, 'Subjects assigned successfully');
     }
