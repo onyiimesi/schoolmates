155f9777aaa8e6697860b5c305dd23ed9b2c2842
onyiimesi <onyedikachukwu62@gmail.com>
2025-10-17 09:54:08 +0100

    Update

--- app/Traits/HttpResponses.php
+++ app/Traits/HttpResponses.php
@@ -4,14 +4,6 @@ namespace App\Traits;
 
 trait HttpResponses
 {
-    /**
-     * Return a successful response with data and message.
-     *
-     * @param mixed $data
-     * @param string|null $message
-     * @param int $code
-     * @return \Illuminate\Http\JsonResponse
-     */
 	protected function success($data, $message = null, $code = 200){
 		return response()->json([
 			'status' => true,
@@ -20,15 +12,7 @@ trait HttpResponses
 		], $code);
 	}
 
-    /**
-     * Return an error response with data and message.
-     *
-     * @param mixed $data
-     * @param string|null $message
-     * @param int $code
-     * @return \Illuminate\Http\JsonResponse
-     */
-	protected function error($data, $message = null, $code = 500){
+	protected function error($data, $message = null, $code = 400){
 		return response()->json([
 			'status' => false,
 			'message' => $message,
@@ -36,16 +20,7 @@ trait HttpResponses
 		], $code);
 	}
 
-    /**
-     * Return a successful paginated response with collection and message.
-     *
-     * @param mixed $collection
-     * @param string|null $message
-     * @param int $code
-     * @param array|null $extraMeta
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function withPagination($collection, string|null $message = null, $code = 200, array|null $extraMeta = [])
+    protected function withPagination($collection, $message = null, $code = 200, ?array $extraMeta = [])
     {
         return response()->json([
             'status' => true,
